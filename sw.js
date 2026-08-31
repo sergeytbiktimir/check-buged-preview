0830443f6e749d09dd6ed1cab39a7c48c3f1ab5e
sbiktimirov <sergey.t.biktimirov@yandex.ru>
2026-08-31 15:49:58 -0400

    deploy: 9b90d4d 2026-08-31T19:49:58.609Z

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chek-budget-pwa-d4e50ffe';
+const CACHE = 'chek-budget-pwa-3544b873';
 const LOCAL = [
   './', 'index.html', 'styles.css', 'app.bundle.js',
   'react.development.js', 'react-dom.development.js',
